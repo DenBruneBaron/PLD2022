26f865fd0b3918ac1a3402d8bb00d6b6369282fc
DenBruneBaron <AskEliasJensen@gmail.com>
2022-02-16 14:17:56 +0100

    ~75% A1 done

---
+++ A1/Cpp_test/cpp_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+#if 0
+/* Giant comment
+ it doesn't matter what I put here */
+
+// it will be ignored forever.
+#endif
