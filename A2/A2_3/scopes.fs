8dc28c839ee1ef7854a9fff853dba419cbb38767
DenBruneBaron <AskEliasJensen@gmail.com>
2022-03-02 20:29:50 +0100

    A2 almost done, pdf reports on the ALGOL60 language added

--- A2/A2_3/scopes.fs
+++ A2/A2_3/scopes.fs
@@ -4,10 +4,7 @@ let rec bizarre b q =
         if b then ( bizarre false mango )
             else ( mango 17 , q 17 );;
 
-printfn "%A value for bizarre" (bizarre)
-
 let dummy = function x -> false
 let bongo = bizarre true dummy;;
 
-printfn "%A value for dummy" (dummy)
 printfn "%A value for bongo" (bongo)
