3f2c14a71bca618e83529371a68b8f4d5486a5db
DenBruneBaron <AskEliasJensen@gmail.com>
2022-03-15 12:47:28 +0100

    more A3 done

--- A3/Java_test/Bingo.java
+++ A3/Java_test/Bingo.java
@@ -1,12 +1,14 @@
+abstract class myAbstractClass {
+    public abstract String bingoString();
+}
 
+class Bingo<T extends myAbstractClass> {
 
-class Bingo<T> {
-
-
-    
-    public void dingo (T t) {
+    public void dingo(T t) {
         System.out.println(t.bingoString());
     }
 
+    public static void main(String[] args) {
 
+    }
 }
--- A3/Java_test/myClass.java
+++
@@ -1,6 +0,0 @@
-public class myClass<T> {
-    
-    public void bingoString() {
-        getClass();
-    }
-}
