1025c41f49e5d485c346a7bb579504a6f47c3c26
DenBruneBaron <AskEliasJensen@gmail.com>
2022-03-19 07:35:34 +0100

    A3 done

--- A3/Bingo.java
+++ A3/Bingo.java
@@ -1,10 +1,18 @@
-import java.util.*;
+class myClass {
+    public String bingoString(){
+        return "This is bingo string, who dis?";
+    }
 
-class Bingo<T> {
-    
-    public void dingo (T t) {
+}
+
+class Bingo<T extends myClass> {
+
+    public void dingo(T t) {
         System.out.println(t.bingoString());
     }
 
-
+    public static void main(String[] args) {
+        Bingo newBingo = new Bingo();
+        newBingo.dingo(new myClass());
+    }
 }
--- A3/Java_test/Bingo.java
+++ A3/Java_test/Bingo.java
@@ -1,14 +1,18 @@
-abstract class myAbstractClass {
-    public abstract String bingoString();
+class myClass {
+    public String bingoString(){
+        return "This is bingo string, who dis?";
+    }
+
 }
 
-class Bingo<T extends myAbstractClass> {
+class Bingo<T extends myClass> {
 
     public void dingo(T t) {
         System.out.println(t.bingoString());
     }
 
     public static void main(String[] args) {
-
+        Bingo newBingo = new Bingo();
+        newBingo.dingo(new myClass());
     }
 }
